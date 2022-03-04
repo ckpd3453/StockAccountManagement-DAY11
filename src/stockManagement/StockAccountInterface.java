b66ff212c08978c4e46c309a870d4f8180e2f5e0
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-04 20:22:11 +0530

    modified due to some commenting done

---
+++ src/stockManagement/StockAccountInterface.java
@@ -0,0 +1,13 @@
+package stockManagement;
+
+public interface StockAccountInterface {
+	
+	double valueOf();
+
+	void buy(int amount, String symbol);
+	
+	void sell(int amount, String symbol);
+	
+	void printReport();
+
+}
